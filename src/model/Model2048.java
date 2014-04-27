c9d9fc637a3382369af89819fbba9d249c16ee4b
odahari87 <odahari@gmail.com>
2014-04-27 23:04:53 +0300

    rotateMatrix change and implementing Up Down Left Right

--- src/model/Model2048.java
+++ src/model/Model2048.java
@@ -21,26 +21,27 @@ public class Model2048 extends Observable implements Model {
 
 	private boolean moveRight() {
 		try {
-			int x = game.length - 1;
-			
+
 			for (int i = 0; i < game.length; i++) {
-				for (int j = x; j > 0; j--) {
-					if (game[i][j] >1 && game[i][j] == game[i][j - 1]) {
+				for (int j = game.length - 1; j > 0; j--) {
+					if (game[i][j] > 1 && game[i][j] == game[i][j - 1]) {
 						game[i][j] = 2 * game[i][j];
 						game[i][j - 1] = -1;
 					} else if (game[i][j] == 0 && game[i][j - 1] != 0) {
 						game[i][j] = game[i][j - 1];
 						game[i][j - 1] = 0;
-						j +=2;
+						if (j < game.length-1) {
+							j += 2;
+						}
 					}
 				}
-				for (int j = 0; j + 1 < game.length; j++) {
-					if (game[i][j] < 2) {
+				for (int j = game.length - 1; j >= 0; j--) {
+					if (game[i][j] == -1) {
 						game[i][j] = 0;
-						if (j > 0) {
-							game[i][j] = game[i][j - 1];
-							game[i][j - 1] = 0;
-						}
+					}
+					while (j > 0 && game[i][j] < 2 && game[i][j - 1] > 0) {
+						game[i][j] = game[i][j - 1];
+						game[i][j - 1] = 0;
 					}
 				}
 			}
@@ -50,63 +51,88 @@ public class Model2048 extends Observable implements Model {
 		}
 
 	}
+
 	private boolean moveLeft() {
 		try {
-			
-			for (int i = 0; i < game.length; i++) {
-				for (int j = 0; j <game.length-1; j++) {
-					if (game[i][j] >1 && game[i][j] == game[i][j + 1]) {
-						game[i][j] = 2 * game[i][j];
-						game[i][j + 1] = -1;
-					} else if (game[i][j] == 0 && game[i][j + 1] != 0) {
-						game[i][j] = game[i][j + 1];
-						game[i][j + 1] = 0;
-						j -=2;
-					}
-				}
-				for (int j = 0; j + 1 < game.length; j++) {
-					if (game[i][j] < 2) {
-						game[i][j] = 0;
-						if (j > 0) {
-							game[i][j] = game[i][j + 1];
-							game[i][j + 1] = 0;
-						
-						}
-					}
-				}
-			}
+			rotateMatrix();
+			rotateMatrix();
+			moveRight();
+			rotateMatrix();
+			rotateMatrix();
+			// for (int i = 0; i < game.length; i++) {
+			// for (int j = 0; j < game.length - 1; j++) {
+			// if (game[i][j] > 1 && game[i][j] == game[i][j + 1]) {
+			// game[i][j] = 2 * game[i][j];
+			// game[i][j + 1] = -1;
+			// } else if (game[i][j] == 0 && game[i][j + 1] != 0) {
+			// game[i][j] = game[i][j + 1];
+			// game[i][j + 1] = 0;
+			// if (j > 0) {
+			// j -= 2;
+			// }
+			//
+			// }
+			// }
+			// for (int j = 1; j < game.length; j++) {
+			// if (game[i][j] == -1) {
+			// game[i][j] = 0;
+			// }
+			//
+			// while (game[i][j - 1] < 2 && game[i][j] > 0 && j > 0) {
+			// game[i][j - 1] = game[i][j];
+			// game[i][j] = 0;
+			// --j;
+			// }
+			//
+			// }
+			// }
 			return true;
 		} catch (Exception e) {
 			return false;
 		}
 
 	}
-	private boolean move(int iTo, int jTo) {
 
-		// for(int i=0;i<maze.length;i++)
-		// {
-		// for(int j=0;j<maze[i].length;j++)
-		// {
-		// if(maze[i][j] == 1)
-		// {
-		// if(i+iTo>-1 && i+iTo<maze.length && j+jTo>-1 &&
-		// j+jTo<maze[i+iTo].length)//range check..
-		// {
-		// if(maze[i+iTo][j+jTo]!=-1)
-		// {
-		// if(maze[i+iTo][j+jTo] == 2)
-		// maze[i+iTo][j+jTo] = 3;//winning mouse
-		// else
-		// maze[i+iTo][j+jTo] = 1;
-		// maze[i][j] = 0;
-		// return true;
-		// }
-		// }
-		// return false;
-		// }
-		// }
-		// }
-		return false;
+	private boolean moveUp() {
+
+		try {
+			rotateMatrix();
+			moveRight();
+			rotateMatrix();
+			rotateMatrix();
+			rotateMatrix();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+
+	}
+
+	private boolean moveDown() {
+		try {
+			rotateMatrix();
+						rotateMatrix();
+			rotateMatrix();
+			moveRight();
+			rotateMatrix();
+			
+			
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+
+	}
+
+	private void rotateMatrix() {
+		int[][] ret = new int[game.length][game.length];
+
+		for (int i = 0; i < game.length; ++i) {
+			for (int j = 0; j < game.length; ++j) {
+				ret[i][j] = game[game.length - j - 1][i];
+			}
+		}
+		game = ret;
 	}
 
 	@Override
@@ -118,21 +144,13 @@ public class Model2048 extends Observable implements Model {
 	public boolean doAction(int userCommand) {
 		switch (userCommand) {
 		case CommandsConsts.UP:
-			// return moveUp();// UP
+			return moveUp();// UP
 		case CommandsConsts.DOWN:
-			//return moveDown();// DOWN
+			return moveDown();// DOWN
 		case CommandsConsts.LEFT:
 			return moveLeft();// LEFT
 		case CommandsConsts.RIGHT:
 			return moveRight();// RIGHT
-		case CommandsConsts.UP_LEFT:
-			return move(-1, -1);// DIAGONAL_UP_LEFT
-		case CommandsConsts.UP_RIGHT:
-			return move(-1, 1);// DIAGONAL_UP_RIGHT
-		case CommandsConsts.DOWN_LEFT:
-			return move(1, -1);// DIAGONAL_DOWN_LEFT
-		case CommandsConsts.DOWN_RIGHT:
-			return move(1, 1);// DIAGONAL_DOWN_RIGHT
 		case CommandsConsts.INITIATE: {
 			this.initiateMaze();
 			return true;
@@ -143,7 +161,7 @@ public class Model2048 extends Observable implements Model {
 
 	private void initiateMaze() {
 		this.game = new int[][] { { 2, 2, 0, 4 }, { 4, 0, 0, 4 },
-				{8, 8, 8, 8 }, { 4, 4, 4, 2 } };
+				{ 8, 8, 8, 8 }, { 4, 4, 4, 2 } };
 	}
 
 }
